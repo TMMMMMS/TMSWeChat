91c177c3459ac724eeb7239bcea61f6887d7787e
TMMMMMS <1003022706@qq.com>
2019-05-18 15:00:04 +0800

    initial configuration

---
+++ TMSWeChat/Constant/TMSConstant.h
@@ -0,0 +1,9 @@
+//
+//  TMSConstant.h
+//  TMSWeChat
+//
+//  Created by TmmmS on 2019/5/18.
+//  Copyright © 2019年 TMS. All rights reserved.
+//
+
+UIKIT_EXTERN const CGFloat TMSStickerTopInset;
---
+++ TMSWeChat/Constant/TMSConstant.m
@@ -0,0 +1,9 @@
+//
+//  TMSConstant.m
+//  TMSWeChat
+//
+//  Created by TmmmS on 2019/5/18.
+//  Copyright © 2019年 TMS. All rights reserved.
+//
+
+CGFloat const TMSStickerTopInset = 12.0;
